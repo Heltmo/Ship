6ea79820fb23465bf629f03c77f289403ec61160
Heltmo <122870357+Heltmo@users.noreply.github.com>
2025-12-27 21:25:14 +0100

    Fix TypeScript errors in supabase server client

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -9,13 +9,13 @@ export function createServerSupabaseClient() {
     process.env.SUPABASE_ANON_KEY ?? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "",
     {
       cookies: {
-        get(name) {
+        get(name: string) {
           return cookieStore.get(name)?.value;
         },
-        set(name, value, options) {
+        set(name: string, value: string, options: any) {
           cookieStore.set({ name, value, ...options });
         },
-        remove(name, options) {
+        remove(name: string, options: any) {
           cookieStore.set({ name, value: "", ...options, maxAge: 0 });
         }
       }
